817c95bb1cde8bbe872ee202d9659df5d25e937a
David Holland <david.w.holland@gmail.com>
2024-10-13 16:27:14 -0400

    bump version number

--- nwmovies.h
+++ nwmovies.h
@@ -1,4 +1,4 @@
-#define _NWMOVIES_VERSION "20171112"
+#define _NWMOVIES_VERSION "20241013"
 #define _NWMOVIES_LOGFILE "nwmovies.log"
 #define _NWMOVIES_PLAYER "ffplay -fs -autoexit -exitonkeydown -exitonmousedown"
 
